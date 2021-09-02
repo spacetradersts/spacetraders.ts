72e80647bdfcef2243e2ecb9feca138d5df68921
SirH <micahbenac52@gmail.com>
2021-09-02 07:32:57 -0400

    fix mjs compiler to output correct exports

--- compileMjs.js
+++ compileMjs.js
@@ -3,9 +3,9 @@ const { readFile, writeFile } = require('fs/promises');
 (async () => {
 try {
 
-    const data = await readFile('dist/src/index.d.ts', { encoding: 'utf-8' });
+    const data = await import('./dist/src/index.js');
 
-    await writeFile('dist/src/index.mjs', data);
+    await writeFile('dist/src/index.mjs', `export { ${Object.keys(data).filter(key => key != '__esModule' && key != 'default').join(',\n')} } from './index.js';`);
 
     const RESTManagerData = await readFile('dist/src/rest/RESTManager.js', { encoding: 'utf-8' });
 
